e1252aed84764b30cec600ae8853dbdfd549f709
Lasha Kvachadze <98225237+Kvachi4@users.noreply.github.com>
2023-03-11 15:33:32 +0400

    Initial commit

---
+++ indoor/indoor.py
@@ -0,0 +1,2 @@
+someText = input("Please writhe some text in uppercase :").lower()
+print(someText)
---
+++ playback/playback.py
@@ -0,0 +1,8 @@
+symbol = "..."
+text = input("Please type few words about you :")
+
+threeDotText = text.replace(" ", "...") 
+
+print(threeDotText)
+
+
